219caae317ee0a9166d6a49c411c53627d6bef69
pradoav <a.rozputnii@gmail.com>
2019-12-17 23:04:59 +0200

    model

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,7 +12,7 @@
 class Category < ApplicationRecord
   has_many :products
 
-  #validates :title,:description, presence: true
+  validates :title,:description, presence: true
   validates :title, uniqueness: true
 
 end
