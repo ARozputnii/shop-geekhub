66f4991ae644c24d708a1ef43f8c5e0b5ae831c7
pradoav <a.rozputnii@gmail.com>
2019-12-23 09:19:19 +0200

    add table category to admin/products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,55 @@
-class CategoriesController < ApplicationController
+class Backoffice::CategoriesController < Backoffice::BackofficeController
+  before_action :find_category, except: %i[new create index]
+
+  def index
+    @categories = if params[:search]
+                    Category.search(params[:search]).order(created_at: :desc).paginate(page: params[:page], per_page: 20)
+                  else
+                    Category.all.order(created_at: :desc).paginate(page: params[:page], per_page: 20)
+                  end
+  end
+
   def show
-    @category = Category.find(params[:id])
     @products = Product.all.order(created_at: :desc).paginate(page: params[:page], per_page: 8)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin: @products
+      flash[:notice] = 'Category has been added'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @category.update_attributes(category_params)
+      redirect_to admin: @products
+      flash[:notice] = 'Category has been edited'
+    else
+      format.html { render :edit }
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_products_path, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title, :description)
   end
 end
