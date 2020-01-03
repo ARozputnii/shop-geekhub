65abad0e42959e9281bc734ed4c2fdb3e1010869
Andrii <a.rozputnii@gmail.com>
2020-01-03 19:09:50 +0200

    scope to product.category_id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,10 +12,15 @@ class ProductsController < ApplicationController
                   Product.all.new_products.paginate(page: params[:page], per_page: 8)
                 elsif params[:sort] == 'old_products'
                   Product.all.old_products.paginate(page: params[:page], per_page: 8)
+                elsif params[:sort] == 'book_id'
+                  Product.all.book_id.paginate(page: params[:page], per_page: 8)
+                elsif params[:sort] == 'magazine_id'
+                  Product.all.magazine_id.paginate(page: params[:page], per_page: 8)
+                elsif params[:sort] == 'manga_id'
+                  Product.all.manga_id.paginate(page: params[:page], per_page: 8)
                 else
                   Product.all.paginate( page: params[:page], per_page: 8)
                 end
-    @categories = Category.all.order(created_at: :desc)
   end
 
   def show
